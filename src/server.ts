556ea8e7d07d414a086d3a25b6e409d9fdf004f2
arpanghosh-dev <arpan.ghosh@weavers-web.com>
2026-01-20 18:24:32 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './app';
+import { connectDB } from './config/db';
+import routes from './routes';
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/api', routes);
+
+const startServer = async () => {
+    await connectDB();
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+};
+
+// PORT=5000
+// MONGO_URI=mongodb+srv://arpan:dbarpanpass@cluster0.ndjticc.mongodb.net/
+
+startServer();
